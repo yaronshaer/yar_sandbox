1511a5841ddfb75f64ebd12eeac008234bd8c9fb
yaron.shaer <yaron.shaer@gmail.com>
2023-04-24 21:36:54 +0200

    minor changes

--- gpt_utils.py
+++ gpt_utils.py
@@ -1,4 +1,4 @@
-# write a function that checks status code from requests and returns status code with tex
+# write a function that checks status code from requests and returns status code with text. please consider 2xx as success and anything else aas error
 def check_status_code(response):
     if response.status_code == 200:
         return response.status_code, "Success"
@@ -15,3 +15,23 @@ def get_text(response):
     if response.json():
         #return the text of the first choice
         return response.json()['choices'][0]['text']
+
+#write a function that checks status code from requests and returns status code with text. please consider 2xx as success and anything else aas error
+def check_status_code(response):
+    #check if status code starts with 2 and has 3 digits
+    if str(response.status_code).startswith('2') and len(str(response.status_code)) == 3:
+        return response.status_code, "Success"
+    else: #return status code and error message that comes from the server
+        return response.status_code, response.json()['error']
+
+
+#check if response contains json object and if yes returns true, otherwise false
+def check_json(response):
+    if response.json():
+        return True
+    else:
+        return False
+
+
+
+
--- script_gpt.py
+++ script_gpt.py
@@ -1,12 +1,14 @@
+import json
 import os
 import requests
 import gpt_utils
 
-api_endpoint=" https://api.openai.com/v1/completions"
-api_key = os.environ['OPENAI_API_KEY']
-prompt = "write a 6 line poem about a dog. the poem should be happy and suitable for children."
-model ="text-davinci-003"
+api_endpoint = "https://api.openai.com/v1/completions"
+# api_key = os.environ['OPENAI_API_KEY']
+api_key = os.getenv("OPENAI_API_KEY")
 
+prompt = "write a 6 line poem about a dog. the poem should be happy and suitable for children."
+model = "text-davinci-003"
 
 request_headers = {
     "Content-Type": "application/json",
@@ -19,9 +21,14 @@ request_data = {
     "max_tokens": 100,
     "temperature": 0.5
 }
-
-
 response = requests.post(api_endpoint, headers=request_headers, json=request_data)
+if gpt_utils.check_json(response):
+    with open("response.json", "w") as outfile:
+        json.dump(response.json(), outfile,indent=4)
+else:
+    print("Something went wrong")
+
+
 print(gpt_utils.check_response(response))
 
 
