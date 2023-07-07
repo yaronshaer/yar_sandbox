aecfd075669e0719d2fff3b8421823a49fab0433
Yaron Shaer <yaron.shaer@klarna.com>
2023-07-07 15:05:43 +0200

    remove test file

--- test_python.py
+++
@@ -1,6 +0,0 @@
-#create a function that takes an input from user by asking for their name and then prints out a greeting to them
-def greeting():
-    name = input("What is your name? ")
-    print("Hello " + name + "!")
-
-greeting()
